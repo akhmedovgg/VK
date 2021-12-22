1e19800cedadbeafe75e864ec241bcb16abc3bf7
Sherzod Akhmedov <cocoexe@icloud.com>
2021-12-23 00:32:06 +0500

    Initial Commit

---
+++ VK iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK iOS
+//
+//  Created by Sherzod Akhmedov on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
